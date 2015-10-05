b0174c991c9d12a382714569837ba3f82df6ba39
Adel Chouanine <a.chouanine@hotmail.fr>
2015-10-05 17:33:14 +0200

    search bar

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,4 +1,10 @@
 class HomeController < ApplicationController
   def index
+  	@users=User.all
+  end
+
+  def search
+  	@users = User.search(params[:query])
+    render "result"
   end
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -3,4 +3,12 @@ class User < ActiveRecord::Base
   # :confirmable, :lockable, :timeoutable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :trackable, :validatable, :confirmable
+
+    def self.search(query)
+    	if query
+      where(["name LIKE ?", "%#{query}%"])
+         else
+            all
+        end
+    end 
 end
