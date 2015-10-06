e749b8f9c94e00368e7b9c6797612a03e2964678
Adel Chouanine <a.chouanine@hotmail.fr>
2015-10-06 16:37:40 +0200

    modification ajout et suppression ami

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -8,6 +8,12 @@ class HomeController < ApplicationController
   
   def show
     @user = User.find(params[:id])
+    @my_friends = User.where(id: current_user.friends.map{|v| [v.user_b_id]})
+    @my_friends.each do |f|
+     if f.id == @user.id
+       @we_are_friends = 1
+     end
+   end 
   end
 
   def search
